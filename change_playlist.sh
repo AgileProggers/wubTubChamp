ed525ba1f9ec7f4af67ce84eae9f50695cee8349
Maxi Quoß <maxi@quoss.org>
2021-05-23 01:19:28 +0200

    check for ytdl

--- change_playlist.sh
+++ change_playlist.sh
@@ -8,5 +8,11 @@ else
        playlist=$1
 fi
 
+# check for youtube-dl
+if ! [ -x "$(command -v youtube-dl)" ]; then
+       echo "youtube-dl not installed"
+       exit 1
+fi
+
 # playlist links to file
 youtube-dl -j --flat-playlist "$playlist" | jq -r '.id' | sed 's_^_https://youtu.be/_' > playlist.txt
